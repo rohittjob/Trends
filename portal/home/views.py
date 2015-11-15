8ca46a87d9d706718a500fa6746e2b83752bf420
Medha <srivastava.medha11@gmail.com>
2015-11-15 22:49:17 +0530

    UI layout created Need to integrate with the database

--- portal/home/views.py
+++ portal/home/views.py
@@ -7,6 +7,3 @@ def index(request):
 	topics_list = TopTrends.objects.order_by('rank')
 	context = {'topics_list': topics_list}
 	return render(request, 'home/index.html', context)
-
-def detail(request, topic_rank):
-    return HttpResponse("Details of topic with rank %s." % topic_rank)
--- portal/portal/urls.py
+++ portal/portal/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
 	url(r'^home/', include('home.urls')),
+	url(r'^details/', include('details.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
