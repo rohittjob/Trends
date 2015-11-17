be943bc152508916e26f1d420c00e5fe5672acbc
Medha <srivastava.medha11@gmail.com>
2015-11-17 13:09:39 +0530

    Minor changes, twitter widget code added

--- portal/portal/urls.py
+++ portal/portal/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-	url(r'^home/', include('home.urls')),
+	url(r'^$', include('home.urls')),
 	url(r'^details/', include('details.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
