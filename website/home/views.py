b48e0d0c88fa4316758ea23e8bbda8765e4c6dcf
Medha <srivastava.medha11@gmail.com>
2016-04-12 12:04:51 +0530

    Alignment corrected

--- website/home/views.py
+++ website/home/views.py
@@ -14,13 +14,13 @@ import unittest
 
 def index(request):
     context = {'hashtags': TopHashTags.objects.order_by('rank'), 'mentions': TopUserMentions.objects.order_by('rank')}
-    tags = make_tags(get_tag_counts("Somewhat abcd abcd abcd abcd abcd surprisingly, a simple low-level hack made a tremendous difference: when constructing the sprite I compressed blocks of 32 1-bit pixels into 32-bit integers, thus reducing the number of checks (and memory) by 32 times.")[:30], maxsize=120, colors=COLOR_SCHEMES['audacity'])
-    data = create_html_data(tags, (440,600), layout=LAYOUT_HORIZONTAL, fontname='PT Sans Regular')
+
+    text = "Somewhat abcd abcd abcd abcd abcd surprisingly, a simple low-level hack made a tremendous difference: when hack constructing the sprite I compressed pixels blocks of 32 1-bit pixels into 32-bit integers, thus reducing hack the number of checks (and memory) by 32 times.";
+    tags = make_tags(get_tag_counts(text)[:30], maxsize=120, colors=COLOR_SCHEMES['audacity'])
+    data = create_html_data(tags, (450,300), layout=LAYOUT_HORIZONTAL, fontname='PT Sans Regular')
         
     template_file = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../templates/home/template.html'), 'r')    
     html_template = Template(template_file.read())
-        
-    context = {}
         
     tags_template = '<li class="cnt" style="top: %(top)dpx; left: %(left)dpx; height: %(height)dpx;"><a class="tag %(cls)s" href="#%(tag)s" style="top: %(top)dpx;\
     left: %(left)dpx; font-size: %(size)dpx; height: %(height)dpx; line-height:%(lh)dpx;">%(tag)s</a></li>'
