90442f7676aa5f36c77e2915b4aeed473413c383
Medha <srivastava.medha11@gmail.com>
2016-04-28 15:31:52 +0530

    Word cloud to image

--- website/home/views.py
+++ website/home/views.py
@@ -11,34 +11,32 @@ from string import Template
 import os
 import time
 import unittest
-
+   
 def index(request):
     context = {'hashtags': TopHashTags.objects.order_by('rank'), 'mentions': TopUserMentions.objects.order_by('rank')}
 
-    text = "Somewhat abcd abcd abcd abcd abcd surprisingly, a simple low-level hack made a tremendous difference: when hack constructing the sprite I compressed pixels blocks of 32 1-bit pixels into 32-bit integers, thus reducing hack the number of checks (and memory) by 32 times.";
-    tags = make_tags(get_tag_counts(text)[:30], maxsize=120, colors=COLOR_SCHEMES['audacity'])
-    data = create_html_data(tags, (450,300), layout=LAYOUT_HORIZONTAL, fontname='PT Sans Regular')
-        
-    template_file = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../templates/home/template.html'), 'r')    
-    html_template = Template(template_file.read())
-        
-    tags_template = '<li class="cnt" style="top: %(top)dpx; left: %(left)dpx; height: %(height)dpx;"><a class="tag %(cls)s" href="#%(tag)s" style="top: %(top)dpx;\
-    left: %(left)dpx; font-size: %(size)dpx; height: %(height)dpx; line-height:%(lh)dpx;">%(tag)s</a></li>'
-        
-    context['tags'] = ''.join([tags_template % link for link in data['links']])
-    context['width'] = data['size'][0]
-    context['height'] = data['size'][1]
-    context['css'] = "".join("a.%(cname)s{color:%(normal)s;}\
-    a.%(cname)s:hover{color:%(hover)s;}" % 
-                                  {'cname':k,
-                                   'normal': v[0],
-                                   'hover': v[1]} 
-                                 for k,v in data['css'].items())
-        
-    html_text = html_template.substitute(context)
-        
-    html_file = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../templates/home/index.html'), 'w')
-    html_file.write(html_text)
-    html_file.close()       
+    #array of words with their frequencies
+    words_arr = ["word1", "word2", "word3", "word4", "word5", "word6", "word7", "word8", "word9", "word10", "word11", "word12", "word13", "word14", "word15", "word16","word17", "word18", "word19", "word20", "word21", "word22", "word23", "word24"]
+    freq_arr = [0.02, 0.02, 0.02, 0.02, 0.02, 0.03, 0.03, 0.03, 0.03, 0.03, 0.04, 0.04, 0.04, 0.04, 0.04, 0.1, 0.1, 0.2, 0.01, 0.05, 0.01, 0.01, 0.01, 0.15]
 
+    #code for generating word cloud
+    word_count = len(words_arr)
+    text = "";
+    for i in range(0,word_count):
+        for j in range(0,(int)(freq_arr[i]*100)):
+            text = text + words_arr[i] + " "
+
+    tags = make_tags(get_tag_counts(text)[:20], maxsize=100, colors=COLOR_SCHEMES['audacity'])  
+
+    output = os.path.join(os.getcwd(), './static/out')
+        
+    if not os.path.exists(output):
+        os.mkdir(output )            
+
+    create_tag_image(tags, os.path.join(output, 'cloud.png'),
+                             size=(900, 600),
+                             background=(0, 0, 0, 255),
+                             layout=LAYOUT_HORIZONTAL, fontname='PT Sans Regular')
     return render(request, 'home/index.html', context)
+
+
